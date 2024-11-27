2224d7b325e5033c7c161ef412f5fc45aa67787f
Sanzdev <bskkeenzo@gmail.com>
2024-11-28 02:23:25 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -25,19 +25,17 @@ const randompass = () => Math.random().toString(36).slice(-8);
 
 const ipClaimData = {}; 
 const checkIpClaimLimit = (req, res, next) => {
-    const ip = req.ip; 
-
+    const ip = req.ip;
+    
     if (!ipClaimData[ip]) {
         ipClaimData[ip] = { claims: 0, lastClaimTime: Date.now() };
     }
 
     const timeDiff = Date.now() - ipClaimData[ip].lastClaimTime;
-
     if (timeDiff > 7 * 24 * 60 * 60 * 1000) { 
         ipClaimData[ip].claims = 0;
         ipClaimData[ip].lastClaimTime = Date.now();
     }
-
     if (ipClaimData[ip].claims >= 2) {
         return res.status(429).json({ message: "You can only claim twice per week." });
     }
